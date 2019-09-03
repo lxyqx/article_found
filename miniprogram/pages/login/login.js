148918811db5728b93e4c13b65f9c5e3af33bc49
unknown <unknown@example.com>
2019-09-04 00:09:05 +0800

    login
    完成了登录和user数据库的录入功能

---
+++ miniprogram/pages/login/login.js
@@ -0,0 +1,225 @@
+// miniprogram/pages/login/login.js
+const app = getApp();
+Page({
+
+  /**
+   * 页面的初始数据
+   */
+  data: {
+    hiddenButton: true
+  },
+
+  onGotUserInfo: function (e) {
+    var _this = this
+    //需要用户同意授权获取自身相关信息
+    if (e.detail.errMsg == "getUserInfo:ok") {
+      //将授权结果写入app.js全局变量
+      app.globalData.auth['scope.userInfo'] = true
+      //尝试获取云端用户信息
+      wx.cloud.callFunction({
+        name: 'get_setUserInfo',
+        data: {
+          getSelf: true
+        },
+        success: res => {
+          if (res.errMsg == "cloud.callFunction:ok")
+            if (res.result) {
+              //如果成功获取到
+              //将获取到的用户资料写入app.js全局变量
+
+              app.globalData.userInfo = res.result.data.userData
+              app.globalData.userId = res.result.data._id
+              wx.navigateTo({
+                url: '/pages/home/home'
+              })
+              //console.log(res)
+            } else {
+              //未成功获取到用户信息
+              //调用注册方法
+              console.log("未注册")
+              _this.register({
+                nickName: e.detail.userInfo.nickName,
+                gender: e.detail.userInfo.gender,
+                avatarUrl: e.detail.userInfo.avatarUrl,
+                tel:'',
+                score:0,
+                money:0,
+                lstid:[],
+                fndid:[],
+                headpic:'',
+                sign:'',
+                birth: ''
+              })
+            }
+        },
+        fail: err => {
+          wx.showToast({
+            title: '请检查网络您的状态',
+            duration: 800,
+            icon: 'none'
+          })
+          console.error("get_setUserInfo调用失败", err.errMsg)
+        }
+      })
+    } else
+      console.log("未授权")
+  },
+
+  register: function (e) {
+    let _this = this
+    wx.cloud.callFunction({
+      name: 'get_setUserInfo',
+      data: {
+        setSelf: true,
+        userData: e
+      },
+      success: res => {
+        if (res.errMsg == "cloud.callFunction:ok" && res.result) {
+          _this.setData({
+            hiddenButton: true
+          })
+          app.globalData.userInfo = e
+          app.globalData.userId = res.result._id
+          //_this.data.registered = true
+          //app.getLoginState()
+          console.log(res)
+          wx.navigateTo({
+            url: '/pages/mine/mine'
+          })
+        } else {
+          console.log("注册失败", res)
+          wx.showToast({
+            title: '请检查网络您的状态',
+            duration: 800,
+            icon: 'none'
+          })
+        }
+      },
+      fail: err => {
+        wx.showToast({
+          title: '请检查网络您的状态',
+          duration: 800,
+          icon: 'none'
+        })
+        console.error("get_setUserInfo调用失败", err.errMsg)
+      }
+    })
+  },
+
+  /**
+   * 生命周期函数--监听页面加载
+   */
+  onLoad: function () {
+    let _this = this
+    //需要用户同意授权获取自身相关信息
+    wx.getSetting({
+      success: function (res) {
+        if (res.authSetting['scope.userInfo']) {
+          //将授权结果写入app.js全局变量
+          app.globalData.auth['scope.userInfo'] = true
+          //从云端获取用户资料
+          wx.cloud.callFunction({
+            name: 'get_setUserInfo',
+            data: {
+              getSelf: true
+            },
+            success: res => {
+              if (res.errMsg == "cloud.callFunction:ok" && res.result) {
+                //如果成功获取到
+                //将获取到的用户资料写入app.js全局变量
+                console.log(res)
+                app.globalData.userInfo = res.result.data.userData
+                app.globalData.userId = res.result.data._id
+                wx.navigateTo({
+                  url: '/pages/home/home'
+                })
+              } else {
+                _this.setData({
+                  hiddenButton: false
+                })
+                console.log("未注册")
+              }
+            },
+            fail: err => {
+              _this.setData({
+                hiddenButton: false
+              })
+              wx.showToast({
+                title: '请检查网络您的状态',
+                duration: 800,
+                icon: 'none'
+              })
+              console.error("get_setUserInfo调用失败", err.errMsg)
+            }
+          })
+        } else {
+          _this.setData({
+            hiddenButton: false
+          })
+          console.log("未授权")
+        }
+      },
+      fail(err) {
+        _this.setData({
+          hiddenButton: false
+        })
+        wx.showToast({
+          title: '请检查网络您的状态',
+          duration: 800,
+          icon: 'none'
+        })
+        console.error("wx.getSetting调用失败", err.errMsg)
+      }
+    })
+  },
+
+
+  /**
+   * 生命周期函数--监听页面初次渲染完成
+   */
+  onReady: function () {
+
+  },
+
+  /**
+   * 生命周期函数--监听页面显示
+   */
+  onShow: function () {
+
+  },
+
+  /**
+   * 生命周期函数--监听页面隐藏
+   */
+  onHide: function () {
+
+  },
+
+  /**
+   * 生命周期函数--监听页面卸载
+   */
+  onUnload: function () {
+
+  },
+
+  /**
+   * 页面相关事件处理函数--监听用户下拉动作
+   */
+  onPullDownRefresh: function () {
+
+  },
+
+  /**
+   * 页面上拉触底事件的处理函数
+   */
+  onReachBottom: function () {
+
+  },
+
+  /**
+   * 用户点击右上角分享
+   */
+  onShareAppMessage: function () {
+
+  }
+})
